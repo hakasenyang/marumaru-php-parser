a9cebd24d4e5b9319f64ee92674b31449c412199
Hakase <hakase@hakase.io>
2018-02-11 13:12:03 +0900

    Update fmaru.php

--- fmaru.php
+++ fmaru.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=500, user-scalable=no">
     <title>fmaru php porting by hakase</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css">
     <!--<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
     rel="stylesheet">-->
     <link href="style.css"
